a68730f3b3fd475028f4e2edce034ea3af2cb898
Vektorfrisk <70740334+Vektorfrisk@users.noreply.github.com>
2022-01-10 14:41:20 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,21 +1,21 @@
 def is_password_valid(password):
     num_count=0
+    upper=0
+    lower=0
     for i in password:
-        if type(i)== int:
-            num_count+=1
-    if num_count>=3:
-        if len(password)>=8:
-            upper=0
-            lower=0
-            for a in password:
-                if a.isupper():
-                    upper+=1
-                if a.islower():
-                    lower+=1
+        if i.isdigit():
+            num_count += 1
+        if i.isupper():
+            upper+=1
+        if i.islower():
+            lower+=1
+      if num_count >= 3:
+         if len(password)>= 8:
             if upper>=1 and lower>=1:
-                print("The password is valid")
+               return(print("Password is valid"))
+                
     else:
-        print("Invalid Password")
+        return(print("Invalid Password"))
 
 
 password1 = input(str("Enter a valid password: "))
